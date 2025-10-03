42589eaada7f2961749231b3f9a8c8d0807c399f
tomass <tomasstraupis@gmail.com>
2025-10-03 20:17:36 +0300

    Pašalinti ant ekrano rodomi valdymo mygtukai. Pataisytas mastelio keitimas liečiamuose ekranuose

--- src/collectUserInput.js
+++ src/collectUserInput.js
@@ -4,22 +4,6 @@ import { findPath } from "./utilies/findPath";
 import { tileSize } from "./constants";
 
 export function collectUserInput(camera, handleZoom) {
-    document
-        .getElementById("forward")
-        ?.addEventListener("click", () => queueMove("forward"));
-
-    document
-        .getElementById("backward")
-        ?.addEventListener("click", () => queueMove("backward"));
-
-    document
-        .getElementById("left")
-        ?.addEventListener("click", () => queueMove("left"));
-
-    document
-        .getElementById("right")
-        ?.addEventListener("click", () => queueMove("right"));
-
     window.addEventListener("keydown", (event) => {
         if (event.key === "ArrowUp") {
             event.preventDefault();
@@ -54,7 +38,8 @@ export function collectUserInput(camera, handleZoom) {
             isPinching = true;
             initialTouchDistance = getTouchDistance(event.touches);
         }
-    });
+        event.preventDefault();
+    }, { passive: false });
 
     window.addEventListener('touchmove', event => {
         if (isPinching && event.touches.length === 2) {
@@ -63,7 +48,8 @@ export function collectUserInput(camera, handleZoom) {
             handleZoom(camera.zoom * zoomFactor);
             initialTouchDistance = currentTouchDistance;
         }
-    });
+        event.preventDefault();
+    }, { passive: false });
 
     window.addEventListener('touchend', () => {
         isPinching = false;
